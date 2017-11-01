168dc0f9b2e46ff2c758b59863fc0163dd117ca8
Quarke <tflanagan94@me.com>
2017-10-31 20:45:56 -0400

    actual index.js

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,8 @@
-var express = require('express')
-var app = express()
-
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
